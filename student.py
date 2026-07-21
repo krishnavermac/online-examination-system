2674c5690744d37bdfa683dadfbba05c767e9f93
Krishna Verma <krishcairns1309@gmail.com>
2026-07-21 19:39:26 +0530

    Remove redundant code in student.py

--- student.py
+++ student.py
@@ -7,7 +7,6 @@ from models import Exam, Question, Result, Answer, db
 
 def init_app(app):
 
-    # student dashboard
     @app.route("/student/dashboard")
     @login_required
     def student_dashboard():
@@ -17,7 +16,6 @@ def init_app(app):
             exams=exams
         )
 
-    # start / submit-exam
     @app.route("/student/start-exam/<int:exam_id>", methods=["GET", "POST"])
     @login_required
     def start_exam(exam_id):
@@ -25,9 +23,7 @@ def init_app(app):
         exam = Exam.query.get_or_404(exam_id)
         questions = Question.query.filter_by(exam_id=exam_id).all()
 
-        # exam start 
         if request.method == "GET":
-            # Store exam start time in session (ANTI-CHEATING)
             session["exam_start_time"] = datetime.now().timestamp()
             session["exam_id"] = exam_id
 
@@ -38,10 +34,8 @@ def init_app(app):
                 duration=exam.duration
             )
 
-        # exam submit
         if request.method == "POST":
 
-            # server side time validation
             start_time = session.get("exam_start_time")
 
             if not start_time:
@@ -52,7 +46,6 @@ def init_app(app):
             if time_taken > exam.duration * 60:
                 return "Time exceeded. Exam auto-submitted."
 
-            # create result
             score = 0
 
             result = Result(
@@ -64,7 +57,6 @@ def init_app(app):
             db.session.add(result)
             db.session.commit()
 
-            # evaluate answers
             for q in questions:
                 selected = request.form.get(f"question_{q.id}")
 
@@ -78,17 +70,14 @@ def init_app(app):
                 )
                 db.session.add(answer)
 
-            # update score
             result.score = score
             db.session.commit()
 
-            # clean session
             session.pop("exam_start_time", None)
             session.pop("exam_id", None)
 
             return redirect(f"/student/result/{result.id}")
 
-    # result page
     @app.route("/student/result/<int:result_id>")
     @login_required
     def result(result_id):
